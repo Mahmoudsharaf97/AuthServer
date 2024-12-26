cd8be2cbc52cef3a6087d49d1c69faa2676d7bcd
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2024-12-26 14:07:07 +0300

    fix dependancy errors

---
+++ Auth_Application/DependencyInjection/ApplicationDependencyInjectionRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Auth_Application.Interface;
+using Auth_Application.Services;
+using Auth_Application.Services.Caching;
+using Auth_Application.Services.Token;
+using IdentityApplication.Interface;
+using IdentityApplication.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Auth_Application.DependencyInjection
+{
+	public static class ApplicationDependencyInjectionRegistration
+	{
+		public static IServiceCollection AddApplicationServicesInjection(this IServiceCollection services)
+		{
+
+			services.AddScoped<IIdentityCachingServices, IdentityCachingServices>();
+			services.AddScoped<IIdentityServices, IdentityServices>();
+			services.AddSingleton<IRefreshTokenService, RefreshTokenService>();
+			services.AddScoped<ISessionServices, SessionServices>();
+			services.AddScoped<ITokenServices, TokenServices>();
+			return services;
+		}
+
+	}
+}
