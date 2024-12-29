81b47b33cfc507a6049e68acf7090179c138b1f0
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2024-12-29 15:50:12 +0300

    match the method definstion with it's implementation

--- Auth_Core/UseCase/Redis/IRedisCaching.cs
+++ Auth_Core/UseCase/Redis/IRedisCaching.cs
@@ -2,10 +2,10 @@
 {
     public interface IRedisCaching
     {
-        Task<string> GetRefreshTokenAsync(string userId);
+        Task<string> GetRefreshTokenAsync(string email);
         Task<bool> SetRefreshTokenAsync(string userId, string tokenValue);
         Task<bool> SetSessionAsync(string userId, SessionStatus session);
-        Task<SessionStatus> GetSessionAsync(string userId);
+        Task<SessionStatus> GetSessionAsync(string email);
         Task<bool> DeleteSessionAsync(string userId);
         bool SetUser(string email, string ninKey, ApplicationUser<string> user);
         //bool SetUserAsync(string email,ApplicationUser<string> user);
