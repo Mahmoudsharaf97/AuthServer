b425eccc4c09696468717be5376151cc0f58265b
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2024-12-30 09:39:03 +0300

    remove versioning as per Eng.Islam requested

--- AuthServer/Endpoints/IdentityEndpoint/IdentityEndpoint.cs
+++ AuthServer/Endpoints/IdentityEndpoint/IdentityEndpoint.cs
@@ -6,39 +6,39 @@ namespace AuthServer.Endpoints.IdentityEndpoint
 	{
 		public static RouteGroupBuilder MapIdentityEndpoints(this RouteGroupBuilder group)
 		{
-			group.MapPost($"/v1/{nameof(ConfirmResetPassword)}", ConfirmResetPassword.Action)
+			group.MapPost($"/{nameof(ConfirmResetPassword)}", ConfirmResetPassword.Action)
 				.WithName("ConfirmResetPassword")
 				.WithSummary("ConfirmResetPassword info");
 
-			group.MapGet($"/v1/{nameof(GatToken)}", GatToken.Action)
+			group.MapGet($"/{nameof(GatToken)}", GatToken.Action)
 				.WithName("GatToken")
 				.WithSummary("GatToken info");
 
-			group.MapPost($"/v1/{nameof(RefreshToken)}", RefreshToken.Action)
+			group.MapPost($"/{nameof(RefreshToken)}", RefreshToken.Action)
 				.WithName("RefreshToken")
 				.WithSummary("RefreshToken info");
 
-			group.MapPost($"/v1/{nameof(LogIn)}", LogIn.Action)
+			group.MapPost($"/{nameof(LogIn)}", LogIn.Action)
 				.WithName("LogIn")
 				.WithSummary("LogIn info");
 			
-			group.MapPost($"/v1/{nameof(LogOut)}", LogOut.Action)
+			group.MapPost($"/{nameof(LogOut)}", LogOut.Action)
 				.WithName("LogOut")
 				.WithSummary("LogOut info");
 			
-			group.MapPost($"/v1/{nameof(Register)}", Register.Action)
+			group.MapPost($"/{nameof(Register)}", Register.Action)
 				.WithName("Register")
 				.WithSummary("Register info");
 			
-			group.MapPost($"/v1/{nameof(ResetPassword)}", ResetPassword.Action)
+			group.MapPost($"/{nameof(ResetPassword)}", ResetPassword.Action)
 				.WithName("ResetPassword")
 				.WithSummary("ResetPassword info");		 
 			
-			group.MapGet($"/v1/{nameof(Captcha)}", Captcha.Action)
+			group.MapGet($"/{nameof(Captcha)}", Captcha.Action)
 				.WithName("Captcha")
 				.WithSummary("Captcha info");	
 
-			group.MapPost($"/v1/{nameof(ValidateCaptcha)}", ValidateCaptcha.Action)
+			group.MapPost($"/{nameof(ValidateCaptcha)}", ValidateCaptcha.Action)
 				.WithName("ValidateCaptcha")
 				.WithSummary("ValidateCaptcha info");
 
