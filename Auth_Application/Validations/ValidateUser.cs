c527631542c4fc878ab8ef8287483b1d61e0e3b9
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2024-12-31 09:32:46 +0300

    edit user validation calss ValidateUser

--- Auth_Application/Validations/ValidateUser.cs
+++ Auth_Application/Validations/ValidateUser.cs
@@ -11,21 +11,52 @@ namespace Auth_Application.Validations
 	{
 		public static ApplicationUser<string> IsValidUser( this ApplicationUser<string> user)
 		{
+			user.IsFound().IsNotDeleted().IsPhoneNumberConfirmed().IsEmailConfirmed().IsLockout();
+			return user;
+		}
+		public static ApplicationUser<string> IsFoundUserNotConfirmed(this ApplicationUser<string> user)
+		{
+			user.IsFound().IsNotDeleted().IsLockout();
+			return user;
+		}
+		public static ApplicationUser<string> IsFoundUserEmailNotConfirmed( this ApplicationUser<string> user)
+		{
+			user.IsFound().IsNotDeleted().IsPhoneNumberConfirmed().IsLockout();
+			return user;
+		}
+		public static ApplicationUser<string> IsFoundUserPhoneNumberNotConfirmed( this ApplicationUser<string> user)
+		{
+			user.IsFound().IsNotDeleted().IsEmailConfirmed().IsLockout();
+			return user;
+		}
+		public static ApplicationUser<string> IsFound( this ApplicationUser<string> user)
+		{
 			if (user == null)
 				throw new AppException(ExceptionEnum.UserNotFound);
-
+			return user;
+		}
+		public static ApplicationUser<string> IsNotDeleted( this ApplicationUser<string> user)
+		{
 			if (user.IsDeleted)
 				throw new AppException(ExceptionEnum.UserDeleted);
-
+			return user;
+		}
+		public static ApplicationUser<string> IsPhoneNumberConfirmed( this ApplicationUser<string> user)
+		{
 			if (!user.PhoneNumberConfirmed)
 				throw new AppException(ExceptionEnum.UserPhoneNotActiveConfirmed);
-
+			return user;
+		}
+		public static ApplicationUser<string> IsEmailConfirmed( this ApplicationUser<string> user)
+		{
 			if (!user.EmailConfirmed)
 				throw new AppException(ExceptionEnum.UserEmailNotconfirmed);
-
-			if (user.LockoutEnd != null)
+			return user;
+		}
+		public static ApplicationUser<string> IsLockout( this ApplicationUser<string> user)
+		{
+			if (user.LockoutEnd != null) // user.LockoutEndDateUtc > DateTime.UtcNow 
 				throw new AppException(ExceptionEnum.UserIsLocked);
-
 			return user;
 
 		}
