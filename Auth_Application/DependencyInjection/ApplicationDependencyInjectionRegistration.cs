c845ba3460fac4d6813b6085ab9684c39870471a
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2025-01-05 13:04:02 +0300

    fix

--- Auth_Application/DependencyInjection/ApplicationDependencyInjectionRegistration.cs
+++ Auth_Application/DependencyInjection/ApplicationDependencyInjectionRegistration.cs
@@ -1,6 +1,7 @@
 ﻿using Auth_Application.Interface;
 using Auth_Application.Services;
 using Auth_Application.Services.Caching;
+using Auth_Application.Services.Captch;
 using Auth_Application.Services.Otp;
 using Auth_Application.Services.Token;
 using Auth_Core.UseCase;
@@ -21,6 +22,7 @@ namespace Auth_Application.DependencyInjection
 			services.AddSingleton<IRefreshTokenService, RefreshTokenService>();
 			services.AddScoped<ISessionServices, SessionServices>();
 			services.AddScoped<ITokenServices, TokenServices>();
+			services.AddScoped<ICaptchService, CaptchService>();
 			services.AddScoped<ICaptchService, CaptchService>();
 			services.AddScoped<IOtpService, OtpService>();
 			return services;
