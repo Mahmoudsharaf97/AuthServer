b485a03e675983f62c2673f9cabc2ffd2a646415
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2025-01-05 13:30:39 +0300

    add Login Input Model for all the LoginMethods

---
+++ Auth_Application/Models/LoginModels/LoginInput/LoginInputModel.cs
@@ -0,0 +1,20 @@
+﻿using Auth_Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth_Application.Models.LoginModels.LoginInput
+{
+	public class LoginInputModel
+	{
+		[Required]
+		public LoginMethod LoginMethod { get; set; }
+		public NormalLoginModel? NormalLoginModel { get; set; }
+		public VerifyLoginYakeenMobile? VerifyLoginYakeenMobile { get; set; }
+		public LoginConfirmationModel? LoginConfirmationModel { get; set; }
+		public VerifyLoginOTPModel? VerifyLoginOTPModel { get; set; }
+	}
+}
