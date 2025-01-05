0d5153a08ead30644a7fd3b1c7e7a9538d8f6db4
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2025-01-05 16:36:30 +0300

    Comment unuesed code

--- Auth_Application/Features/LogIn/BeginLogin/Queries/BeginEndLoginQuery.cs
+++ Auth_Application/Features/LogIn/BeginLogin/Queries/BeginEndLoginQuery.cs
@@ -1,11 +1,11 @@
-﻿using Auth_Application.Models;
-using MediatR;
+﻿//using Auth_Application.Models;
+//using MediatR;
 
-namespace Auth_Application.Features
-{
-    public class BeginLoginQuery : IRequest<LogInOutput>
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
+//namespace Auth_Application.Features
+//{
+//    public class BeginLoginQuery : IRequest<LogInOutput>
+//    {
+//        public string Email { get; set; }
+//        public string Password { get; set; }
+//    }
+//}
--- Auth_Application/Features/LogIn/BeginLogin/Queries/BeginLoginHandler.cs
+++ Auth_Application/Features/LogIn/BeginLogin/Queries/BeginLoginHandler.cs
@@ -1,23 +1,23 @@
-﻿using Auth_Application.Models;
-using IdentityApplication;
-using IdentityApplication.Interface;
-using MediatR;
+﻿//using Auth_Application.Models;
+//using IdentityApplication;
+//using IdentityApplication.Interface;
+//using MediatR;
 
-namespace Auth_Application.Features
-{
-	public class BeginLoginHundler : IRequestHandler<BeginLoginQuery, LogInOutput>
-    {
-        public IIdentityServices _identityServices { get; }
-        public BeginLoginHundler(IIdentityServices identityServices)
-        {
-            _identityServices = identityServices;
-        }
+//namespace Auth_Application.Features
+//{
+//	public class BeginLoginHundler : IRequestHandler<BeginLoginQuery, LogInOutput>
+//    {
+//        public IIdentityServices _identityServices { get; }
+//        public BeginLoginHundler(IIdentityServices identityServices)
+//        {
+//            _identityServices = identityServices;
+//        }
 
 
-        public async Task<LogInOutput> Handle(BeginLoginQuery request, CancellationToken cancellationToken)
-        {
-          var input = MapperObject.Mapper.Map<LogInInput>(request); // test success
-            return await _identityServices.BeginLogin(input);
-        }
-    }
-}
+//        public async Task<LogInOutput> Handle(BeginLoginQuery request, CancellationToken cancellationToken)
+//        {
+//          var input = MapperObject.Mapper.Map<LogInInput>(request); // test success
+//            return await _identityServices.BeginLogin(input);
+//        }
+//    }
+//}
