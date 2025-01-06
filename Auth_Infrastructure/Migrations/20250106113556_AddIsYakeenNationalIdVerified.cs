04aa99fc4b858ded10913f6bf921c91d078a536a
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2025-01-06 14:39:30 +0300

    add new migration

---
+++ Auth_Infrastructure/Migrations/20250106113556_AddIsYakeenNationalIdVerified.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Auth_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsYakeenNationalIdVerified : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsYakeenNationalIdVerified",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsYakeenNationalIdVerified",
+                table: "AspNetUsers");
+        }
+    }
+}
